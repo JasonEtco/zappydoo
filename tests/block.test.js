ec473429e10573ee473096fd545dcc5fb24e51a6
Jason Etcovitch <jasonetco@gmail.com>
2018-02-05 16:11:19 -0500

    Simplify identifier

--- tests/block.test.js
+++ tests/block.test.js
@@ -4,7 +4,7 @@ const IfElse = require('../lib/if-else')
 describe('block', () => {
   describe('identify', () => {
     it('returns an instance of IfElse', () => {
-      expect(Block.identify('{% if thing %}...{% endif %}')).toBeInstanceOf(IfElse)
+      expect(Block.identify('if')).toBeInstanceOf(IfElse)
     })
   })
 })
